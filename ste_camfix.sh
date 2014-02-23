bf78ae593e4fd8f7f7003453372db145431225a7
Tesla-MADAL93 <tesla.madal93@gmail.com>
2014-02-23 16:49:00 +0100

    Delete ste_camfix.sh

--- ste_camfix.sh
+++
@@ -1,5 +0,0 @@
-echo -e $CL_BLU"Cherrypicking Camera fix"$CL_RST
-cd packages/apps/Camera2
-git fetch https://github.com/CyanogenMod/android_packages_apps_Camera2 cm-11.0
-git cherry-pick 42067bbce2203088e09039169b0262691dd07e97
-cd ../../..
